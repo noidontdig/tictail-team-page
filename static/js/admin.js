6f40811d8860748409cf35e55d600ad65f36fc87
noidontdig <alexandraqin@gmail.com>
2015-09-18 00:11:21 -0400

    Finish admin

--- static/js/admin.js
+++ static/js/admin.js
@@ -17,16 +17,20 @@ var TeamManager = React.createClass({
       }.bind(this)
     });
   },
+
   handleDelete: function (id, index) {
     var team = this.state.team;
     team.splice(index, 1);
     this.setState({team: team});
     this.apiRequest(this.props.url + '/' + id, 'DELETE', function () {});
   },
+
   handleEdit: function (index) {
     var member = this.state.team[index];
     this.setState({member: member});
+    this.showForm();
   },
+
   // Handles both creating and updating a team member.
   handleSubmit: function () {
     var isNew = !this.state.member.id;
@@ -39,29 +43,61 @@ var TeamManager = React.createClass({
         var newTeam = [member].concat(team);
         this.setState({team: newTeam});
       }
-      this.setState({member: {}});
+      this.hideForm();
     }
     this.apiRequest(url, type, callback, this.state.member);
   },
+
   handleFieldChange: function (member) {
     this.setState({member: member});
   },
-  getInitialState: function () {
-    return {team: [], member: {}};
+
+  handleAdd: function () {
+    this.setState({member: {}});
+    this.showForm();
   },
+
+  handleCancel: function () {
+    this.hideForm();
+  },
+
+  showForm: function () {
+    this.setState({showForm: true, hasError: false}, function () {
+      document.getElementById('first_name').focus();
+    });
+  },
+
+  hideForm: function () {
+    this.setState({showForm: false, member: {}, hasError: false});
+  },
+
+  handleError: function () {
+    this.setState({hasError: true});
+  },
+
+  getInitialState: function () {
+    return {team: [], member: {}, showForm: false, hasError: false};
+  },
+
   componentDidMount: function () {
     this.apiRequest(this.props.url, 'GET', function (data) {
       this.setState({team: data});
     });
   },
+
   render: function () {
     return (
       <div className="teamManager">
         <h1>Manage the Tictail team</h1>
-        <button className="add">Add new member</button>
-        <TeamMemberForm onFieldChange={this.handleFieldChange}
-                        onMemberSubmit={this.handleSubmit}
-                        member={this.state.member} />
+        <button className="add button primary" onClick={this.handleAdd}>+ Add new member</button>
+        {this.state.showForm ? <TeamMemberForm onFormError={this.handleError}
+                                               onFieldChange={this.handleFieldChange}
+                                               onMemberSubmit={this.handleSubmit}
+                                               onCancel={this.handleCancel}
+                                               member={this.state.member}
+                                               hasError={this.state.hasError} />
+                              : null}
+
         <TeamMemberList onEditMember={this.handleEdit}
                         onDeleteMember={this.handleDelete}
                         team={this.state.team} />
@@ -88,20 +124,22 @@ var TeamMemberList = React.createClass({
                     firstName={teamMember.first_name}
                     lastName={teamMember.last_name}
                     teamName={teamMember.team}
-                    location={teamMember.location}
                     id={teamMember.id} />
       );
     }, this);
     return (
       <table className="teamMemberList">
-        <tr>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Team</th>
-          <th>Location</th>
-          <th>Edit</th>
-        </tr>
-        {teamMemberNodes}
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th className="teamCell">Team</th>
+            <th>Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teamMemberNodes}
+        </tbody>
       </table>
     );
   }
@@ -123,11 +161,10 @@ var TeamMember = React.createClass({
       <tr className="teamMember" id={this.props.id} ref="teamMember">
         <td>{this.props.firstName}</td>
         <td>{this.props.lastName}</td>
-        <td>{this.props.teamName}</td>
-        <td>{this.props.location}</td>
+        <td className="teamCell">{this.props.teamName}</td>
         <td>
-          <button className="edit" onClick={this.handleEdit}>Edit</button>
-          <button className="delete" onClick={this.handleDelete}>Delete</button>
+          <button className="edit button" onClick={this.handleEdit}>Edit</button>
+          <button className="delete button" onClick={this.handleDelete}>Delete</button>
         </td>
       </tr>
     );
@@ -137,14 +174,14 @@ var TeamMember = React.createClass({
 var TeamMemberForm = React.createClass({
   handleSubmit: function (event) {
     event.preventDefault();
+
     if (!this.props.member.first_name ||
         !this.props.member.last_name ||
         !this.props.member.team ||
         !this.props.member.color ||
         !this.props.member.image ||
         !this.props.member.location) {
-      // TODO: better error handling
-      alert('Invalid Entry.');
+      this.props.onFormError();
       return;
     }
     this.props.onMemberSubmit();
@@ -155,18 +192,40 @@ var TeamMemberForm = React.createClass({
     this.props.onFieldChange(editedMember);
     return;
   },
+  handleCancel: function (event) {
+    event.preventDefault();
+    this.props.onCancel();
+  },
   render: function () {
     return (
       <form className="teamMemberForm" onSubmit={this.handleSubmit}>
-        <input id="first_name" type="text" placeholder="First Name" value={this.props.member.first_name} onChange={this.handleChange}/>
-        <input id="last_name" type="text" placeholder="Last Name" value={this.props.member.last_name} onChange={this.handleChange}/>
+        <label for="first_name">First Name</label>
+        <input id="first_name" type="text" placeholder="First Name *" value={this.props.member.first_name} onChange={this.handleChange} autofocus/>
+
+        <label for="last_name">Last Name</label>
+        <input id="last_name" type="text" placeholder="Last Name *" value={this.props.member.last_name} onChange={this.handleChange}/>
+
+        <label for="title">Title</label>
         <input id="title" type="text" placeholder="Title" value={this.props.member.title} onChange={this.handleChange}/>
-        <input id="team" type="text" placeholder="Team" value={this.props.member.team} onChange={this.handleChange}/>
-        <input id="color" type="text" placeholder="Color (hex code)" value={this.props.member.color} onChange={this.handleChange}/>
-        <input id="image" type="text" placeholder="Avatar URL" value={this.props.member.image} onChange={this.handleChange}/>
-        <input id="location" type="text" placeholder="Location" value={this.props.member.location} onChange={this.handleChange}/>
+
+        <label for="team">Team</label>
+        <input id="team" type="text" placeholder="Team *" value={this.props.member.team} onChange={this.handleChange}/>
+
+        <label for="color">Color</label>
+        <input id="color" type="text" placeholder="Color (ex: '000000') *" value={this.props.member.color} onChange={this.handleChange}/>
+
+        <label for="image">Image URL</label>
+        <input id="image" type="text" placeholder="Image URL *" value={this.props.member.image} onChange={this.handleChange}/>
+
+        <label for="Location">Location</label>
+        <input id="location" type="text" placeholder="Location *" value={this.props.member.location} onChange={this.handleChange}/>
+
         <input type="text" ref="id" hidden value={this.props.member.id} onChange={this.handleChange}/>
-        <input type="submit" value="Submit" />
+        {this.props.hasError ? <p className="error">Invalid submission. Please fill in all mandatory fields (*).</p> : null}
+        <div>
+          <input className="submit primary button" type="submit" value="Submit" />
+          <button className="cancel button" onClick={this.handleCancel}>Cancel</button>
+        </div>
       </form>
     );
   }
